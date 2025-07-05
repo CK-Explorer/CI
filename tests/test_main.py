bb971b60599dbc06bb43e8adc177f67c5614a53f
CK-Explorer <121228064+CK-Explorer@users.noreply.github.com>
2025-07-05 20:15:31 +0800

    Added files for testing.

---
+++ tests/test_main.py
@@ -0,0 +1,10 @@
+import pytest
+from package.main import addition
+
+@pytest.mark.parametrize("a, b, c", [
+    (5, 6, 11),
+    (10, 8, 18),
+    (1, 1, 2)
+])
+def test_addition(a, b, c):
+    assert c == addition(a,b)
