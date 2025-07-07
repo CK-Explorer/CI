ce11fbc6e3f67c20b35b0f1eeb816f89afc18d4a
CK-Explorer <121228064+CK-Explorer@users.noreply.github.com>
2025-07-07 23:47:21 +0800

    fix: testing

--- package/main.py
+++ package/main.py
@@ -5,7 +5,7 @@ def subtraction(a: int, b: int) -> int:
     return a - b
 
 def multiplication(a: int, b: int) -> int:
-    return a * b
+    return a*b
 
 def modulus(a: int, b: int) -> int:
     return a%b
