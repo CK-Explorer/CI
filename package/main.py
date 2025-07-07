64501ce580d4e082ab6ca47d6ae8aff9e9d92c93
CK-Explorer <121228064+CK-Explorer@users.noreply.github.com>
2025-07-07 21:21:59 +0800

    fix: Wrong function name

--- package/main.py
+++ package/main.py
@@ -4,5 +4,5 @@ def addition(a: int, b: int) -> int:
 def subtraction(a: int, b: int) -> int:
     return a - b
 
-def subtraction(a: int, b: int) -> int:
+def multiplication(a: int, b: int) -> int:
     return a * b
